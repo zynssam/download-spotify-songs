600925dfd6b87c3d3ca062d0b5e36f7ae9212bda
zynssam <24020104410@reva.edu.in>
2024-10-18 21:00:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import commands.getspotify as getspotify 
+import commands.getytlinks as getlinks
+import commands.download as download
+import commands.linkgetter as linkgetter
+
+folder_location = r'' #Your Folder Location
+def get_spotify_playlist_id(url):
+    parts = url.split('/')
+    
+    if 'playlist' in parts:
+        return parts[parts.index('playlist') + 1].split('?')[0]
+    return None
+
+def download_tracks():
+    url = linkgetter.create_url_input_app()
+
+    track_names = getspotify.getspotipysongs(get_spotify_playlist_id(url), )
+    for track_name in track_names:
+        try:
+            yt_link = getlinks.get_youtube_link(track_name)
+            if yt_link:
+                print(f"Downloading: {track_name} from {yt_link}")
+                download.download_audio(yt_link, folder_location)
+                print(f"Downloaded: {track_name}")
+            else:
+                print(f"No YouTube link found for: {track_name}")
+        except Exception as e:
+            print(f"Error downloading {track_name}: {e}")
+
+
+if __name__ == "__main__":
+    download_tracks()
